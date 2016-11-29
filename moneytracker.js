a36f814757a7ce34161ea99ffad45f84afff1da4
Oleg <olsard7@gmail.com>
2016-11-29 14:57:18 +0300

    I use modified foreach but it does not work. I gonna create a branch and test why do foreach not wok

--- moneytracker.js
+++ moneytracker.js
@@ -1,5 +1,39 @@
 function randomUsd(min, max){
     var minCent = min*100;
     var maxCent = max*100;
-    return Math.floor(Math.random()*(max-min)+min)
+    var amount = Math.floor(Math.random()*(maxCent-minCent) + minCent);
+    return amount/100;// we work with cents
 }
+
+function randomByn(min, max){
+    var minByn = min*100;
+    var maxByn = max*100;
+    var amount =  Math.floor(Math.random()*(maxByn-minByn) + minByn);
+    return amount/100; // we work with cents
+}
+
+function randomByr(min, max){
+    var minByr = min/100;
+    var maxByr = max/100;
+    var amount = Math.floor(Math.random()*(maxByr - minByr) + minByr);
+    return amount*100; // we work with 100 rub minimum
+
+}
+
+// console.log(randomByr(1000,5000));
+
+function oneDayOfUser(){
+    var userDay = [], i = 0;
+    db.studdent.find().forEach(
+        i++;
+        function(myName){
+            userDay[i] = myName.OperationName;
+            print("Operation Name: " + myName.OperationName);
+            print("i= " + i);
+        }
+
+    );
+    
+}
+
+oneDayOfUser();
